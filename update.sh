92d1abd1f4b81ed4c125f0a5eaa08da079575905
Josh Hill <jamesjoshuahill@gmail.com>
2014-12-22 10:57:24 +0000

    Add .gemrc

--- update.sh
+++ update.sh
@@ -7,14 +7,17 @@ cp ~/.git-completion.bash .
 cp ~/.gitconfig .
 cp ~/.gitignore .
 
-# Bundler
-cp ~/.bundle/config ./.bundle/
+# Brew
+brew list > brew-list.txt
 
 # rbenv
 cp ~/.rbenv/default-gems ./.rbenv/
 
+# Rubygems
+cp ~/.gemrc .
+
+# Bundler
+cp ~/.bundle/config ./.bundle/
+
 # Sublime Text 2
 cp ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/*.sublime-settings sublime2/
-
-# Brew
-brew list > brew-list.txt
