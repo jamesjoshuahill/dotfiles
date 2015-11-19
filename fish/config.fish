842142236d307247e64a95b15a058347ea0e1aab
Josh Hill <josh@cloudcredo.com>
2015-11-19 17:55:39 +0000

    Update fish config and prompt

--- fish/config.fish
+++ fish/config.fish
@@ -1,21 +1,37 @@
+# rbenv
 status --is-interactive; and . (rbenv init -|psub)
 
-# Aliases
+# go
+set -x GOPATH $HOME/go
+
+# aliases
 alias bake='bundle exec rake'
 alias be='bundle exec'
 alias bi='bundle install'
 alias es='exercism submit'
 alias ef='exercism fetch'
-alias fs='foreman start'
 alias irb='irb -r irb/completion'
-alias j='jekyll serve'
 alias ll='ls -alG'
-alias ls='ls -G'
 alias o='open .'
 alias r='rspec'
-alias rs='rspec spec'
-alias rt='rake test'
 alias s='subl .'
 alias sbp='subl ~/.bash_profile'
 alias sgc='subl ~/.gitconfig'
+alias sfc='subl ~/.config/fish/config.fish'
 alias t='gittower .'
+
+# thefuck
+function fuck -d 'Correct your previous console command'
+    set -l exit_code $status
+    set -l eval_script (mktemp 2>/dev/null ; or mktemp -t 'thefuck')
+    set -l fucked_up_commandd $history[1]
+    thefuck $fucked_up_commandd > $eval_script
+    . $eval_script
+    rm $eval_script
+    if test $exit_code -ne 0
+        history --delete $fucked_up_commandd
+    end
+end
+
+# direnv
+eval (direnv hook fish)
