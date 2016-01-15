3043fa1f7bee21baa20a5e594f3863e781a1ce83
Josh Hill <josh@cloudcredo.com>
2016-01-15 14:05:04 +0000

    Add Atom config

--- backup.sh
+++ backup.sh
@@ -15,3 +15,11 @@ cp ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Default\ \(OSX\
 cp ~/.config/fish/config.fish ./fish/
 cp ~/.config/fish/fishd.* ./fish/
 cp ~/.config/fish/functions/*.fish ./fish/functions/
+
+# Atom
+cp ~/.atom/config.cson ./atom/
+cp ~/.atom/init.coffee ./atom/
+cp ~/.atom/keymap.cson ./atom/
+cp ~/.atom/snippets.cson ./atom/
+cp ~/.atom/styles.less ./atom/
+ls -d ~/.atom/packages/*/ | cut -d'/' -f6 > ./atom/atom-packages.txt
