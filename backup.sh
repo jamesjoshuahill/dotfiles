1d6229350f117b1409809a4c92e0bc28fa512245
Josh Hill <josh@cloudcredo.com>
2016-01-15 09:43:42 +0000

    Remove unused config

--- backup.sh
+++ backup.sh
@@ -1,22 +1,11 @@
 #! /bin/bash
 
-cp ~/.bash_profile .
-
 # Git
 cp ~/.gitconfig .
 cp ~/.gitignore .
 
 # Brew
 brew list > brew-list.txt
-
-# rbenv
-cp ~/.rbenv/default-gems ./.rbenv/
-
-# Rubygems
-cp ~/.gemrc .
-
-# Bundler
-cp ~/.bundle/config ./.bundle/
 
 # Sublime Text 2
 cp ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/*.sublime-settings sublime2/
