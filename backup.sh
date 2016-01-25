5758d2b5ede231df29fc5219cc124e04274ba28b
Josh Hill <jamesjoshuahill@gmail.com>
2016-01-25 14:42:19 +0000

    Formatting fixes to backup script

--- backup.sh
+++ backup.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+# Fish
+cp ~/.config/fish/config.fish fish/
+cp ~/.config/fish/fishd.* fish/
+cp ~/.config/fish/functions/*.fish fish/functions/
+
 # Git
 cp ~/.gitconfig .
 cp ~/.gitignore .
@@ -11,15 +16,10 @@ brew list > brew-list.txt
 cp ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/*.sublime-settings sublime2/
 cp ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Default\ \(OSX\).sublime-keymap sublime2/
 
-# Fish shell config
-cp ~/.config/fish/config.fish ./fish/
-cp ~/.config/fish/fishd.* ./fish/
-cp ~/.config/fish/functions/*.fish ./fish/functions/
-
 # Atom
-cp ~/.atom/config.cson ./atom/
-cp ~/.atom/init.coffee ./atom/
-cp ~/.atom/keymap.cson ./atom/
-cp ~/.atom/snippets.cson ./atom/
-cp ~/.atom/styles.less ./atom/
-ls -d ~/.atom/packages/*/ | cut -d'/' -f6 > ./atom/atom-packages.txt
+cp ~/.atom/config.cson atom/
+cp ~/.atom/init.coffee atom/
+cp ~/.atom/keymap.cson atom/
+cp ~/.atom/snippets.cson atom/
+cp ~/.atom/styles.less atom/
+ls -d ~/.atom/packages/*/ | cut -d'/' -f6 > atom/atom-packages.txt
