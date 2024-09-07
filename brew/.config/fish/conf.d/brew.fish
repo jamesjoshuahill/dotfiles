acef1c58699b41daf9aa86e5df739bb12892bcea
Josh Hill <jamesjoshuahill@users.noreply.github.com>
2024-09-07 09:16:36 +0100

    Ensure homebrew bin is before /bin in PATH

--- brew/.config/fish/conf.d/brew.fish
+++ brew/.config/fish/conf.d/brew.fish
@@ -1,5 +1,7 @@
 /opt/homebrew/bin/brew shellenv | source
 
+fish_add_path -g /opt/homebrew/bin /opt/homebrew/sbin
+
 set -x HOMEBREW_NO_ENV_HINTS 1
 set -x HOMEBREW_NO_ANALYTICS 1
 set -x HOMEBREW_NO_INSECURE_REDIRECT 1
