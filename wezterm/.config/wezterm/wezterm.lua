a7aeb1871f3a494aa9b00f1cc9ec56b262750acc
Josh Hill <jamesjoshuahill@users.noreply.github.com>
2025-11-25 11:54:13 +0000

    Disable quit when all WezTerm windows are closed

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -9,6 +9,7 @@ config.color_scheme = 'Dracula'
 config.font = wezterm.font 'SFMono Nerd Font'
 config.font_size = 18.0
 config.line_height = 1.2
+config.quit_when_all_windows_are_closed = false
 
 config.keys = {
   {key="Enter", mods="SHIFT", action=wezterm.action{SendString="\x1b\r"}},
