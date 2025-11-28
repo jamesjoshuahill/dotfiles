e7e5ad2166a546a16857857899e456e18ecfd463
Josh Hill <jamesjoshuahill@users.noreply.github.com>
2025-11-28 17:36:05 +0000

    Add Wezterm shortcuts for panes

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -12,7 +12,50 @@ config.line_height = 1.2
 config.quit_when_all_windows_are_closed = false
 
 config.keys = {
-  {key="Enter", mods="SHIFT", action=wezterm.action{SendString="\x1b\r"}},
+	-- Support Shift+Enter in Claude Code
+	{
+		key = "Enter",
+		mods = "SHIFT",
+		action = wezterm.action {
+			SendString="\x1b\r"
+		},
+	},
+	-- Create new horizontal split pane
+	{
+		key = 'd',
+		mods = 'CMD',
+		action = wezterm.action.SplitHorizontal {
+			domain = 'CurrentPaneDomain'
+		},
+	},
+	-- Create new vertical split pane
+	{
+		key = 'D',
+		mods = 'CMD|SHIFT',
+		action = wezterm.action.SplitVertical {
+			domain = 'CurrentPaneDomain'
+		},
+	},
+	-- Select next pane
+	{
+		key = '[',
+		mods = 'CMD',
+		action = wezterm.action.ActivatePaneDirection 'Next',
+	},
+	-- Select previous pane
+	{
+		key = ']',
+		mods = 'CMD',
+		action = wezterm.action.ActivatePaneDirection 'Prev',
+	},
+	-- Close current pane with confirm
+	{
+		key = 'w',
+		mods = 'CMD',
+		action = wezterm.action.CloseCurrentPane {
+			confirm = true
+		},
+	},
 }
 
 -- and finally, return the configuration to wezterm
