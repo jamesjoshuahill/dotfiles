39a9e1093d838bbc213947a18eff71080d8b30f8
Josh Hill <jamesjoshuahill@users.noreply.github.com>
2026-01-07 13:44:01 +0000

    Fallback to cat when bat is not available
    When the fish shell starts it uses cat to load theme config.
    This happens before bat is added to the PATH.

--- bat/.config/fish/functions/cat.fish
+++ bat/.config/fish/functions/cat.fish
@@ -1,3 +1,8 @@
-function cat
-  command bat --style=header,grid $argv
+function cat --wraps bat
+  if command -q bat
+    command bat --style=header,grid $argv
+    return
+  end
+
+  command cat $argv
 end
